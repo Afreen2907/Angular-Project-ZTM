3e7853a68af706bdc149480ddf8662c25b67741c
Afreen2907 <afreen_fajila@outlook.com>
2022-05-26 16:55:36 +0800

    adding environment

--- basics/src/main.ts
+++ basics/src/main.ts
@@ -1,6 +1,11 @@
+import { enableProdMode } from '@angular/core'; //turns on production mode
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
+if (environment.production) {
+  enableProdMode();
+}
 //loading of platform (JIT) 
 //loading of app
 platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err))
