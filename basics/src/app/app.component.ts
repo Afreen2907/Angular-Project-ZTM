151725176323470f96d1accc3ba5c7645c2a4c6e
Afreen2907 <afreen_fajila@outlook.com>
2022-05-27 17:41:54 +0800

    Pipe example

--- basics/src/app/app.component.ts
+++ basics/src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
 
-  name = 'Luis'
+  name = 'luis ramirez'
   imageURL = 'https://picsum.photos/id/237/500/500'
 
   getName() {
