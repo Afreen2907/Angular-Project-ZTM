b8bd26a493d3cf411cc139a652b4e8ac62261f45
Afreen2907 <afreen_fajila@outlook.com>
2022-06-02 11:50:44 +0800

    Toggling of Modal

--- clips/src/app/shared/modal/modal.component.ts
+++ clips/src/app/shared/modal/modal.component.ts
@@ -8,11 +8,15 @@ import { ModalService } from 'src/app/services/modal.service';
 })
 export class ModalComponent implements OnInit {
 
-  constructor(private modal: ModalService) {
+  constructor(public modal: ModalService) {
     
    }
 
   ngOnInit(): void {
   }
 
+  closeModal() {
+
+    this.modal.toggleModal()
+  } 
 }
