2632fb6e0817e2b7d2082440f3f3ce0f6978d98a
Afreen2907 <afreen_fajila@outlook.com>
2022-06-04 22:44:05 +0800

    pushing async and handling memory leak

--- RxJs/src/index.js
+++ RxJs/src/index.js
@@ -1,12 +1,19 @@
 import { Observable } from 'rxjs'
 
 const observable = new Observable((subscriber) => {
-    subscriber.next('Hello World')
-    subscriber.error('Error!')
-    subscriber.next('test')
-
+    // subscriber.next('test')
+    const id = setInterval(() => {
+        subscriber.next('test')
+        console.log('leak') //testing of memory leak
+    }, 1000)
     subscriber.complete()
+
+    return () => {
+        clearInterval(id)
+    }
 })
+
+console.log('before')
 
 observable.subscribe({
     next: (value) => {
@@ -19,3 +26,5 @@ observable.subscribe({
         console.error(err)
     },
 })
+
+console.log('after')
