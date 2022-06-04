186af621affe263dc3514c21570f41a7c018e3e0
Afreen2907 <afreen_fajila@outlook.com>
2022-06-04 23:20:39 +0800

    DOM event operator

--- RxJs/src/index.js
+++ RxJs/src/index.js
@@ -1,9 +1,5 @@
-// import { interval } from 'rxjs'
-
-// const observable = interval(1000)
-
-import { timer } from 'rxjs'
-const observable = timer(100)
+import { fromEvent } from 'rxjs'
+const observable = fromEvent(document, 'click')
 
 const subscription = observable.subscribe(console.log)
 
