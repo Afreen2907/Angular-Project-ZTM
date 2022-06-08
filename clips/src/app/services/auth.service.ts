1e0548d6d0daa1541134e35f66165264d6b2624d
Afreen2907 <afreen_fajila@outlook.com>
2022-06-08 09:37:18 +0800

    Enable and disable nav bar based on login

--- clips/src/app/services/auth.service.ts
+++ clips/src/app/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import IUser from '../models/user.model';
 
 @Injectable({
@@ -9,12 +11,16 @@ import IUser from '../models/user.model';
 export class AuthService {
 
   private usersCollection: AngularFirestoreCollection<IUser>;
+  public isAuthenticated$: Observable<boolean>
 
   constructor(
     private auth: AngularFireAuth,
     private db: AngularFirestore
   ) {
-    this.usersCollection = db.collection('user')
+    this.usersCollection = db.collection('users')
+    this.isAuthenticated$ = auth.user.pipe(
+      map(user => !!user)
+    )
    }
 
   public async creatorUser(userData: IUser) {
@@ -28,11 +34,20 @@ export class AuthService {
     const userCred = await this.auth.createUserWithEmailAndPassword(
      userData.email, userData.password
     )
-     this.usersCollection.add({
+
+    if (!userCred.user) {
+      throw new Error("User can't be found")
+    }
+
+    await this.usersCollection.doc(userCred.user.uid).set({
        name: userData.name,
        email: userData.email,
        age: userData.age,
        phoneNumber: userData.phoneNumber
-     })
+    })
+    
+    await userCred.user.updateProfile({
+      displayName:userData.name
+    })
   }
 }
