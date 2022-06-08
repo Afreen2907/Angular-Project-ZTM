00f29abe6ec49af00f85957bb762bf5121cfbf4e
Afreen2907 <afreen_fajila@outlook.com>
2022-06-08 10:59:57 +0800

    sign out function added

--- clips/src/app/nav/nav.component.ts
+++ clips/src/app/nav/nav.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { ModalService } from '../services/modal.service';
-
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -12,7 +12,8 @@ export class NavComponent implements OnInit {
   
   constructor(
     public modal: ModalService,
-    public auth: AuthService
+    public auth: AuthService,
+    private afAuth: AngularFireAuth 
   ) { 
    
   }
@@ -25,6 +26,14 @@ export class NavComponent implements OnInit {
 
     this.modal.toggleModal('auth')
   }
+
+ async logout($event: Event) {
+    $event.preventDefault()
+
+   await this.afAuth.signOut()
+  }
+
+
 
 
 }
