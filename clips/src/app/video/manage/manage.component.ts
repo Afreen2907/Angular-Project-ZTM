1b8add5de28f97ccee7b67f977be8fe558319cb2
Afreen2907 <afreen_fajila@outlook.com>
2022-06-09 14:02:34 +0800

    more on query params

--- clips/src/app/video/manage/manage.component.ts
+++ clips/src/app/video/manage/manage.component.ts
@@ -14,13 +14,18 @@ export class ManageComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.route.queryParamMap.subscribe((params: Params) => {
-      this.videoOrder = params.sort === '2' ? params.sort : '1'
-    })
+    console.log("ngOnInit")
+    this.route.queryParamMap.subscribe(
+      (params: Params) => {
+      this.videoOrder = params.params.sort === '2' ? params.params.sort : '1'
+    },
+    
+    )
   }
 
-  sort($event: Event) {
-    const { value } = ($event.target as HTMLSelectElement)
+  sort(event: Event) {
+    const { value } = (event.target as HTMLSelectElement)
+    
     this.router.navigate([], {
       relativeTo: this.route,
       queryParams: {
