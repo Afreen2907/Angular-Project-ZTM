6259754d177eb240b2634d5221d2dfccc62c1c59
Afreen2907 <afreen_fajila@outlook.com>
2022-06-09 14:11:03 +0800

    wildcard routes

--- clips/src/app/video/video-routing.module.ts
+++ clips/src/app/video/video-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClipComponent } from '../clip/clip.component';
+import { NotFoundComponent } from '../not-found/not-found.component';
 import { ManageComponent } from './manage/manage.component';
 import { UploadComponent } from './upload/upload.component';
 
@@ -21,6 +22,10 @@ const routes: Routes = [{
   {
     path: 'clip/:id',
     component: ClipComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
   }
 ];
 
